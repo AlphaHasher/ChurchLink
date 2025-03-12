3ee15d35b014097af3b13f956e09c0a177914d12
not.bass <34872213+bassnp@users.noreply.github.com>
2025-03-12 13:23:53 -0700

    Login and Signup Auth

--- frontend/app/lib/firebase/firebase_auth_service.dart
+++ frontend/app/lib/firebase/firebase_auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+/// Another test commit
 /// - Connect to login page
 /// - Connect to sign up page
 /// - Connect to user profile
