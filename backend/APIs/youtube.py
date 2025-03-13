4bd68383eaa8ff77652bfc9eececeb3d812f4ab3
dylancochran <dylancochran@csus.edu>
2025-03-12 22:01:50 -0700

    Stashing changes for merge prep

--- backend/APIs/youtube.py
+++ backend/APIs/youtube.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
+import requests
 from googleapiclient.discovery import build
+
 
 load_dotenv()
 
@@ -14,29 +16,46 @@ PRIMARY_CHANNEL_ID = os.getenv("PRIMARY_CHANNEL_ID")
 youtubeClient = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
 
 
-#The function checkYoutubeChannel returns a dictionary with 2 keys.
-#isStreaming is a boolean, a simple true/false that will tell you if your requested channel is currently streaming
-#streamLink is a string, it will send a link to the livestream. It will return None if the channel is not currently streaming.
-def checkYoutubeChannel(channelID):
 
-    retDict = {"isStreaming":False, "streamLink":None}
+class YoutubeHelper:
+    #The function checkYoutubeChannel returns a dictionary with 2 keys.
+    #isStreaming is a boolean, a simple true/false that will tell you if your requested channel is currently streaming
+    #streamLink is a string, it will send a link to the livestream. It will return None if the channel is not currently streaming.
+    def checkYoutubeChannel(channelID):
 
-    request = youtubeClient.search().list(
-        part="id",
-        channelId=channelID,
-        eventType="live",
-        type="video"
-    )
-    response = request.execute()
+        retDict = {"isStreaming":False, "streamLink":None}
 
-    if "items" in response and len(response["items"]) > 0:
-        video_id = response["items"][0]["id"]["videoId"]
-        retDict["isStreaming"] = True
-        retDict["streamLink"] = f"https://www.youtube.com/watch?v={video_id}"
-    
-    return retDict
+        request = youtubeClient.search().list(
+            part="id",
+            channelId=channelID,
+            eventType="live",
+            type="video"
+        )
+        response = request.execute()
+
+        if "items" in response and len(response["items"]) > 0:
+            video_id = response["items"][0]["id"]["videoId"]
+            retDict["isStreaming"] = True
+            retDict["streamLink"] = f"https://www.youtube.com/watch?v={video_id}"
+        
+        return retDict
 
 
-# Test Code Below, uncomment and optionally replace the channel ID with a livestreaming channel to check:
-# This will be deleted later, it was just used by me to ensure that the API call functions
-#print(checkYoutubeChannel(PRIMARY_CHANNEL_ID))
+    # Test Code Below, uncomment and optionally replace the channel ID with a livestreaming channel to check:
+    # This will be deleted later, it was just used by me to ensure that the API call functions
+    #print(checkYoutubeChannel(PRIMARY_CHANNEL_ID))
+
+
+    def subscribeToMainNotifications():
+        HUB_URL = "https://pubsubhubbub.appspot.com/subscribe"
+        CALLBACK_URL = "https://yourdomain.com/youtube/live"
+        data = {
+        "hub.mode": "subscribe",
+        "hub.topic": f"https://www.youtube.com/xml/feeds/videos.xml?channel_id={PRIMARY_CHANNEL_ID}",
+        "hub.callback": CALLBACK_URL,
+        "hub.lease_seconds": 864000,  # 10 days
+        }
+        response = requests.post(HUB_URL, data=data)
+        print(response.status_code, response.text)
+
+    subscribeToMainNotifications()
