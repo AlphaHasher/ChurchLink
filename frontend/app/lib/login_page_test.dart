87a01a5c7d3310e5e9fc25ceeba3a7927272981f
desarso <malek.gabriel33@gmail.com>
2025-03-14 00:08:02 -0700

    wow those keys are painful

--- frontend/app/lib/login_page_test.dart
+++ frontend/app/lib/login_page_test.dart
@@ -61,7 +61,6 @@ class _LoginPageState extends State<LoginPage> {
             ElevatedButton(
               onPressed: () async {
                 String? token = await _authService.signInWithGoogle(); // ✅ Fix: Now returns a Future<String?>
-
                 if (token != null && token.isNotEmpty) {  // ✅ Ensure token is not null/empty
                   print("✅ Google Sign-In Successful! Token: $token");
 
