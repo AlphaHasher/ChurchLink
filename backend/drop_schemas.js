54213232bd02d4358b3dafb6bdbf5c857a64fcff
desarso <malek.gabriel33@gmail.com>
2025-04-10 16:15:20 -0700

    getting rid of temp drop schema files

--- backend/drop_schemas.js
+++
@@ -1,8 +0,0 @@
-// Switch to SSBC_DB database
-db = db.getSiblingDB('SSBC_DB');
-
-// Drop all indexes on events collection
-db.events.dropIndexes();
-
-// Create new index on ministry field
-db.events.createIndex({ ministry: 1 }, { unique: true, name: "unique_ministry_index" });
