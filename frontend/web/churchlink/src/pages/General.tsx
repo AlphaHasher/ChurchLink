6fdd2b1da853e9593ae722d1b665d70999d3b694
RyoRim <taipham164@gmail.com>
2025-09-03 13:45:48 -0700

    clean up file after merge with housecleaner

--- frontend/web/churchlink/src/pages/General.tsx
+++
@@ -1,9 +0,0 @@
-function General({ name = "Home" }: { name: string }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-    </div>
-  );
-}
-
-export default General;
--- frontend/web/churchlink/src/types/MyPermsRequest.tsx
+++
@@ -1,5 +0,0 @@
-export type MyPermsRequest = {
-    user_assignable_roles: boolean;
-    event_editor_roles: boolean;
-    user_role_ids: boolean,
-};
