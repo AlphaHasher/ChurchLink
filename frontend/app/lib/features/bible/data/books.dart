55295f18989cba0d4cab0429955ea4b1450889e1
Rylan Pereira <rylanrpcollege@gmail.com>
2025-09-04 21:28:23 -0700

    adjust comments to indicate which bible revision is used

--- frontend/app/lib/features/bible/data/books.dart
+++ frontend/app/lib/features/bible/data/books.dart
@@ -3,5 +3,8 @@
 // the Bible, as stored in books.json. 
 // TODO: Implementation. Move away from individually listed Bible books
 // TODO: in separate files and consolidate it. 
+// TODO: Implement the Bible revisions
+// TODO: KJV: "1769 Blayney (public domain)"
+// TODO: RST: "1876 (modern orthography)"
 // -----------------------------------------------------------------------------
 
