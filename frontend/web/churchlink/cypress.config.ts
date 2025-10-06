81ef45998819423089567ef0f4a672a9b66fe622
AndrewCoats22 <coolcoats@hotmail.com>
2025-10-05 22:30:38 -0700

    Added basic home page test for cypress, way easier to implement

--- frontend/web/churchlink/cypress.config.ts
+++ frontend/web/churchlink/cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: 'cypress/support/e2e.ts',
+    video: true,
+    retries: { runMode: 2, openMode: 0 }, // mild flake control
+    screenshotOnRunFailure: true,
   },
 });
