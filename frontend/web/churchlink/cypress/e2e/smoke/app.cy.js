4b40ce8ae8b9c8737ad33959b534e256f32d2cc4
dylancochran <dylancochran@csus.edu>
2025-11-22 00:51:43 -0800

    attack of the clones

--- frontend/web/churchlink/cypress/e2e/smoke/app.cy.js
+++
@@ -1,8 +0,0 @@
-describe('App shell', function () {
-    it('loads home', function () {
-        cy.visit('/');
-        cy.findByRole('navigation').should('exist');
-        // tbf we dont have a true home page yet so just use the login
-        cy.findByRole('link', { name: /login/i }).should('exist');
-    });
-});
