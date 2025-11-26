2d7eb50bb2a05a090ce12e2487eb68e17eb5d7b4
AndrewCoats22 <coolcoats@hotmail.com>
2025-11-25 19:09:05 -0800

    minimal changes needed.

--- frontend/app/lib/pages/user/legal_terms.dart
+++ frontend/app/lib/pages/user/legal_terms.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:app/models/legal_model.dart';
 import 'package:app/helpers/legal_helper.dart';
 
